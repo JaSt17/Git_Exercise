4124aa4073ce28e4fafcf67aedeb997668f0e9ef
Jaro <ja2332st-s@student.lu.se>
2024-02-19 11:15:43 +0100

    Main hi

--- python_hi.py
+++ python_hi.py
@@ -3,4 +3,5 @@ import random
 print (" Hello world ")
 if random . random () > 0.5:
 print (" Again : Hello world !!")
-print (" Hello world ")  .py
+print (" Hello world ")
+print ("Hello from main")
